7ca05826c812736eb9881a9f69f4ba7340f95b9f
aljobiju <aljovadackel@gmail.com>
2024-03-20 15:55:43 +0530

    login,profile ui done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { Box } from "@mui/joy";
+import {
+  experimental_extendTheme as materialExtendTheme,
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  THEME_ID as MATERIAL_THEME_ID,
+} from "@mui/material/styles";
+import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Login from "./pages/Login/Login";
+import Header from "./components/Common/Header";
+import MyProfile from "./pages/Profile/Profile";
 
+const materialTheme = materialExtendTheme();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
+      <JoyCssVarsProvider>
+        <CssBaseline enableColorScheme />
+        <Box>
+          <MyProfile />
+        </Box>
+      </JoyCssVarsProvider>
+    </MaterialCssVarsProvider>
   );
 }
 
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import CssBaseline from "@mui/joy/CssBaseline";
+import LeftComponent from "../../components/Login/LeftComponent";
+import RightComponent from "../../components/Login/RightComponent";
+
+// function ModeToggle() {
+//   const { mode, setMode } = useColorScheme();
+//   const [mounted, setMounted] = React.useState(false);
+
+//   // necessary for server-side rendering
+//   // because mode is undefined on the server
+//   React.useEffect(() => {
+//     setMounted(true);
+//   }, []);
+//   if (!mounted) {
+//     return <Button variant="soft">Change mode</Button>;
+//   }
+
+//   return (
+//     <Button
+//       variant="soft"
+//       onClick={() => {
+//         setMode(mode === 'light' ? 'dark' : 'light');
+//       }}
+//     >
+//       {mode === 'light' ? 'Turn dark' : 'Turn light'}
+//     </Button>
+//   );
+// }
+
+export default function Login() {
+  return (
+    <main style={{ display: "flex", justifyContent: "center" }}>
+      {/* <ModeToggle /> */}
+      <CssBaseline />
+      <LeftComponent />
+      <RightComponent />
+    </main>
+  );
+}
